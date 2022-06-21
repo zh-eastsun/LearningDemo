8ab4ea573c1f2432ba5d976d40ebd3ee2cefe52d
zhangdongyang <zh_eastsun@163.com>
2022-06-21 13:03:12 +0800

    [Feature] Python 字符串

--- Python/src/Main.py
+++ Python/src/Main.py
@@ -2,6 +2,7 @@
 
 # 第一个注释
 # 导入模块
+from base64 import encode
 import keyword
 from operator import truediv
 from re import X
@@ -40,8 +41,60 @@ if __name__ == "__main__":
     
     # 数字类型
     # 1. 整形，Python3中只有int型，没有Long
-    num = 1     # 普通10进制整形
+    # 普通10进制整形
+    num = 1
     print("num的类型是", type(num))
     
-    num = 10_000_000      # 可以使用_对整形进行分割，这里只是为了提升可读性
+    # 可以使用_对整形进行分割，这里只是为了提升可读性
+    num = 10_000_000
     print("num的数值是", num)
+    
+    # 2. 16进制数
+    num = 0x1234
+    print("num(16)的值是", num)
+    #------------------
+    
+    # 字符串
+    # 1. 单行字符串
+    str = "这是一个单行字符串"
+    
+    # 2. 多行字符串
+    str = """这
+    是
+    一
+    个
+    多
+    行
+    字
+    符
+    串
+    """
+    
+    # 3. 字符串转义
+    str = "Hello, world!\n"
+    print(str)
+    # 4. 不需要转义
+    str = r"Hello, world!\n"
+    # 5. 表示'\'
+    str = "Hello, world!\\n"
+    print(str)
+    
+    # 6. 字符串格式化
+    print("""
+          第一个参数：%d,
+          第二个参数：%f,
+          第三个参数：%s
+          """ %(1, 2.0, "三"))
+    # 7. ord获取字符的整数表现形式
+    ascii = 'A'
+    print(ord(ascii))
+    
+    # 8. chr获取对应ASCII编码的字符
+    ascii = 66
+    print(chr(ascii))
+    
+    # 9. 对字符串进行编码
+    str = "hello world"
+    print(str.encode("ascii"))  # ascii只能对英文进行编码
+    str = "中文"
+    print(str.encode("utf-8"))  # 编码中文需要支持中文的字符集
